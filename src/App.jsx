2502f83ee572e8177b1488902589e4ec3f11a0e6
ducsangtran <ducsang.1505@gmail.com>
2024-05-23 21:35:24 +0700

    table users

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,35 @@
-const App = () => {
-  return (
-    <div>App</div>
-  )
+import React, { useState } from "react";
+import UserManagement from "./components/UserManagement";
+import Dashboard from "./components/Dashboard";
+import { Nav, Navbar, Container } from "react-bootstrap";
+
+function App() {
+    const [page, setPage] = useState("dashboard");
+    const [totalUsers, setTotalUsers] = useState(0); // Khởi tạo totalUsers
+
+    return (
+        <div>
+            <Navbar bg="dark" variant="dark" expand="lg">
+                <Container>
+                    <Navbar.Brand href="#">Jewelry Auction Admin</Navbar.Brand>
+                    <Nav className="me-auto">
+                        <Nav.Link onClick={() => setPage("dashboard")}>
+                            Dashboard
+                        </Nav.Link>
+                        <Nav.Link onClick={() => setPage("users")}>
+                            User Management
+                        </Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
+            {page === "dashboard" && <Dashboard totalUsers={totalUsers} />}{" "}
+            {/* Truyền totalUsers vào Dashboard */}
+            {page === "users" && (
+                <UserManagement setTotalUsers={setTotalUsers} />
+            )}{" "}
+            {/* Truyền setTotalUsers vào UserManagement */}
+        </div>
+    );
 }
 
-export default App
+export default App;
